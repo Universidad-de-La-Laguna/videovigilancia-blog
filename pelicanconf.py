96687afc4f65e4fc2550aa95cc28241d37a0886e
Jesús Torres <jmtorres@ull.es>
2013-01-11 22:03:57 +0000

    configurar los feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 
 AUTHOR = u'Jesús Torres'
 SITENAME = u'Sistema de Videovigilancia from Scratch'
-SITEURL = ''
+SITEURL = 'http://ull-etsii-sistemas-operativos.github.com/videovigilancia-blog'
 
 TIMEZONE = 'Atlantic/Canary'
 
@@ -11,6 +11,11 @@ DEFAULT_LANG = u'es'
 
 FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
 SUMMARY_MAX_LENGTH = 200
+
+# Feeds
+FEED_DOMAIN = SITEURL
+FEED='feeds/atom.xml'
+FEED_RSS='feeds/rss.xml'
 
 # Blogroll
 LINKS =  ()
