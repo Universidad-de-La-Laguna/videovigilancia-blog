de7ae4dc4a57aafb478cb53a22f2c6dbf107ac71
Jesús Torres <jmtorres@ull.es>
2013-01-11 21:44:04 +0000

    revisar archivo de configuración para las opciones más comunes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u'Jes\xfas Torres'
+AUTHOR = u'Jesús Torres'
 SITENAME = u'Sistema de Videovigilancia from Scratch'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Atlantic/Canary'
 
 DEFAULT_LANG = u'es'
 
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+SUMMARY_MAX_LENGTH = 200
+
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/ull-etsii-sistemas-operativos/'),
+          ('google+', 'http://goo.gl/5hufC'),)
 
 DEFAULT_PAGINATION = 10
 
