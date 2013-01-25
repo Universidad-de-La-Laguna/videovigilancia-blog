c1b3b2d2902cdd759f5e3e457f073bcacb8dee39
Jesús Torres <jmtorres@ull.es>
2013-01-25 12:58:41 +0000

    Update documentation

--- theme/js/pluscomments.js
+++ theme/js/pluscomments.js
@@ -1,54 +1,63 @@
-var commentr = commentr || {};
-var apiKey = "AIzaSyClPfZd7RRcX9e3eYiOLV__FyRO9RmJzmg";
+var pluscomments = pluscomments || {};
 
-function commentrLoad() {
-  gapi.client.load('plus', 'v1', commentr.go);
+pluscomments.load = function(apiKey) {
+  gapi.client.load('plus', 'v1', pluscomments.go);
   gapi.client.setApiKey(apiKey);
 }
 
 // search for g-comments-for classes
-commentr.go = function() {
+pluscomments.go = function() {
   //  Find the comment elements to process
   var fetchElements = document.getElementsByClassName('g-comments-for');
   for (var i=0; i < fetchElements.length; i++) {
     var activityId = fetchElements[i].classList[1];
-    commentr.fetchComments(activityId);
+    pluscomments.fetchComments(activityId);
   }
 }
 
-commentr.fetchComments = function(activityId) {
-  var request = gapi.client.plus.comments.list({
+pluscomments.fetchComments = function(activityId) {
+  var rpcBatch = gapi.client.newRpcBatch();
+  var activitiesGet = gapi.client.plus.activities.get({
+    'activityId': activityId,
+  });
+  var commentsList = gapi.client.plus.comments.list({
     'activityId': activityId,
     'maxResults': '100'
   });
-  request.execute(commentr.parseComments);
+  rpcBatch.add(activitiesGet, {
+    'id': 'activity'
+  });
+  rpcBatch.add(commentsList, {
+    'id': 'comments'
+  });
+  rpcBatch.execute(pluscomments.parseComments);
 }
 
-commentr.parseComments = function(responseJson) {
-  if (typeof(responseJson.items) == "undefined") return;
-
-  var activity = responseJson.items[0].inReplyTo[0];
-  var comments = responseJson.items;
+pluscomments.parseComments = function(responseJson) {
+  var activity = responseJson['activity'].result;
+  var comments = responseJson['comments'].result.items;
 
   //find element to insert into
   var insertionElements =
       document.getElementsByClassName('g-comments-for ' + activity.id);
   var insertionElement = insertionElements[0];
 
-  var newContents = "";
-  for (i = 0; i < comments.length; i++) {
-    var actor = comments[i].actor;
+  var innerHTML = "";
+  if (typeof(comments) != "undefined") {
+    var newContents = "";
+    for (i = 0; i < comments.length; i++) {
+      var actor = comments[i].actor;
 
-    var commentBody = comments[i].object.content;
+      var commentBody = comments[i].object.content;
 
-    //do the insertion
-    newContents += "<dt><a href='" + actor.url + "'><img src='" +
-        actor.image.url + "'></a></dt>" + "<dd><a href='" + actor.url + "'>" +
-        actor.displayName + "</a>: " + commentBody + "</dd>";
-
+      //do the insertion
+      newContents += "<dt><a href='" + actor.url + "'><img src='" +
+          actor.image.url + "'></a></dt>" + "<dd><a href='" + actor.url + "'>" +
+          actor.displayName + "</a>: " + commentBody + "</dd>";
+    }
+    innerHTML = "<dl>" + newContents + "</dl> ";
   }
-  insertionElement.innerHTML = "<dl>" + newContents + "</dl> " +
+  insertionElement.innerHTML = innerHTML +
       "<p class='g-commentlink'>Por favor, comenta a través del <a href='" +
       activity.url + "'>artículo en Google+</a>.</p>";
 }
-
